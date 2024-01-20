c51024c9ab871bde589ae330faa789266e5ac88f
A'zamjon Abdumuxtorov <azamjonabdumuxtorov1220@gmail.com>
2024-01-20 23:41:23 +0500

    Initial Commit

---
+++ FirePerfect/FirePerfectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirePerfectApp.swift
+//  FirePerfect
+//
+//  Created by A'zamjon Abdumuxtorov on 20/01/24.
+//
+
+import SwiftUI
+
+@main
+struct FirePerfectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
